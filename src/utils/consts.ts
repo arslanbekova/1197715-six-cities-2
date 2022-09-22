cdfc4acb88d20e9e99ba96eafe6d77ab62385dcf
Liudmila Arslanbekova <ludmila.belenko@gmail.com>
2022-09-23 01:01:38 +0400

    added IoC

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -50,3 +50,9 @@ export enum City {
   Hamburg = 'Hamburg',
   Dusseldorf = 'Dusseldorf',
 }
+
+export const Component = {
+  Application: Symbol.for('Application'),
+  LoggerInterface: Symbol.for('LoggerInterface'),
+  ConfigInterface: Symbol.for('ConfigInterface'),
+} as const;
