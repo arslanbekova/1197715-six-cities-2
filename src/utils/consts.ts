264a6a50bf5a11fef23f5e708f709a708eaf7d3c
Liudmila Arslanbekova <ludmila.belenko@gmail.com>
2022-10-02 14:07:21 +0400

    added database settings

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -55,4 +55,5 @@ export const Component = {
   Application: Symbol.for('Application'),
   LoggerInterface: Symbol.for('LoggerInterface'),
   ConfigInterface: Symbol.for('ConfigInterface'),
+  DatabaseInterface: Symbol.for('DatabaseInterface')
 } as const;
